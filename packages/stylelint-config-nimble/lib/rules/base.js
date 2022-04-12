165b884fa35187f01beb66cc0f48b85033244ba0
Hoang Mirs <hoang.mirs@gmail.com>
2022-04-12 17:13:52 +0700

    [#3] Add base rules

--- packages/stylelint-config-nimble/lib/rules/base.js
+++ packages/stylelint-config-nimble/lib/rules/base.js
@@ -1,5 +1,10 @@
+const sortOrderSmacss = require('stylelint-config-property-sort-order-smacss/generate')
+
 module.exports = {
-  plugins: [],
-  extends: [],
-  rules: {}
+  plugins: ['stylelint-scss', 'stylelint-order'],
+  extends: ['stylelint-config-sass-guidelines'],
+  rules: {
+    'order/properties-alphabetical-order': null,
+    'order/properties-order': [sortOrderSmacss({ emptyLineBefore: 'always' })]
+  }
 }
