c4a79f16d22b62ea74bcb1a9a1c4b2221c9b73e1
Hoang Mirs <hoang.mirs@gmail.com>
2022-04-12 18:01:34 +0700

    [#3] Add more rules based on Compass

--- packages/stylelint-config-nimble/lib/rules/base.js
+++ packages/stylelint-config-nimble/lib/rules/base.js
@@ -4,7 +4,9 @@ module.exports = {
   plugins: ['stylelint-scss', 'stylelint-order'],
   extends: ['stylelint-config-sass-guidelines'],
   rules: {
+    'function-calc-no-unspaced-operator': true,
+    'max-nesting-depth': 3,
     'order/properties-alphabetical-order': null,
-    'order/properties-order': [sortOrderSmacss({ emptyLineBefore: 'always' })]
+    'order/properties-order': [sortOrderSmacss({ emptyLineBefore: 'always' })],
   }
 }
