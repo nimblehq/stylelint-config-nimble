d9623ca36bcbd0c0cb62b464e9c7aff34f34792b
Hoang Mirs <hoang.mirs@gmail.com>
2023-05-08 21:43:43 +0700

    [#12] Update Node version to the LTS 18

--- packages/stylelint-config-nimble/.eslintrc.js
+++ packages/stylelint-config-nimble/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['@nimblehq/eslint-config-nimble'],
+  extends: ['@nimblehq/eslint-config-nimble-core'],
   rules: {
     '@typescript-eslint/no-var-requires': 'off',
   },
--- packages/stylelint-config-nimble/lib/index.js
+++ packages/stylelint-config-nimble/lib/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
 module.exports = {
-  extends: [
-    './rules/base',
-  ].map(require.resolve),
+  extends: ['./rules/base'].map(require.resolve),
 };
